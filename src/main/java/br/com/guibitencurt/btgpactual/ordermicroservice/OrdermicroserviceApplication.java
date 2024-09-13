a1a8700681c8afe905f0cebeb05864223786e5fb
Guilherme <guilhermebitencurt7@gmail.com>
2024-09-13 16:33:13 -0300

    initial commit

---
+++ src/main/java/br/com/guibitencurt/btgpactual/ordermicroservice/OrdermicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.guibitencurt.btgpactual.ordermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermicroserviceApplication.class, args);
+	}
+
+}
